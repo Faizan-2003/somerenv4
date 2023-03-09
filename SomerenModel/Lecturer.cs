d67d6b6cac63290616b86c3decc95cfa087b68d7
Yani16 <yanidaniel76@yahoo.ro>
2023-03-09 12:37:55 +0100

    Code for assignment1

---
+++ SomerenModel/Lecturer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SomerenModel
+{
+    public class Lecturer
+    {
+        public int lecturerId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int telephone { get; set; }
+        public int age { get; set; }
+    }
+}
