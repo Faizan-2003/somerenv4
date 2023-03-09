418c23ac3653590236c3804c49691ebc1f82c652
Yani16 <yanidaniel76@yahoo.ro>
2023-03-09 14:41:06 +0100

    I give up

--- SomerenDAL/LecturerDao.cs
+++ SomerenDAL/LecturerDao.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using SomerenModel;
 
 namespace SomerenDAL
 {
-    internal class LecturerDao
+    public class LecturerDao : BaseDao
     {
+        public List<Lecturer> GetAllLecturers()
+        {
+            string query = "SELECT LecturerId, firstName, lastName, telephone, age, roomId FROM LECTURER";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Lecturer> ReadTables(DataTable dataTable)
+        {
+            List<Lecturer> lecturers = new List<Lecturer>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Lecturer lecturer = new Lecturer()
+                {
+                    lecturerId = (int)dr["LecturerId"],
+                    firstName = dr["firstName"].ToString(),
+                    lastName= dr["lastName"].ToString(),
+                    telephone = (int)dr["telephone"],
+                    age = (int)dr["age"],
+                    roomId = (int)dr["roomId"]
+                };
+                lecturers.Add(lecturer);
+            }
+            return lecturers;
+        }
     }
 }
---
+++ SomerenService/LecturerService.cs
@@ -0,0 +1,22 @@
+﻿using SomerenDAL;
+using SomerenModel;
+using System.Collections.Generic;
+
+namespace SomerenService
+{
+    public class LecturerService
+    {
+        private LecturerDao lecturerdb;
+
+        public LecturerService()
+        {
+            lecturerdb = new LecturerDao();
+        }
+        public List<Lecturer> GetLecturers()
+        {
+            List<Lecturer> lecturers = lecturerdb.GetAllLecturers();
+            return lecturers;
+        }
+
+    }
+}
