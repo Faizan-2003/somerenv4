a86b40202e2be90ae2b53dac440d023433727c99
Yani16 <yanidaniel76@yahoo.ro>
2023-03-17 13:34:16 +0100

    Finish

--- SomerenService/LecturerService.cs
+++ SomerenService/LecturerService.cs
@@ -6,14 +6,17 @@ namespace SomerenService
 {
     public class LecturerService
     {
+        // creating field lecturerdb form class LecturerDao
         private LecturerDao lecturerdb;
 
         public LecturerService()
         {
+            // new LecturerDao from LecturerDao lecturerdb
             lecturerdb = new LecturerDao();
         }
         public List<Lecturer> GetLecturers()
         {
+            // getting lecturer data from the database
             List<Lecturer> lecturers = lecturerdb.GetAllLecturers();
             return lecturers;
         }
