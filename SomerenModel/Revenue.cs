d2129e88ea130bf293247a2e69499a08ec352f98
PhonkPetros <113183622+PhonkPetros@users.noreply.github.com>
2023-03-23 13:48:31 +0100

    bubba

---
+++ SomerenModel/Revenue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Revenue
+{
+    public string drinkName { get; set; }   // Name of the drink
+    public string drinkType { get; set; }    // Alcoholic or Non-Alcoholic
+    public float price { get; set; }  // Price of the drink
+    public float overallProfit { get; set; }
+    public int stock { get; set; } // Total quantity sold
+    public DateTime dateTime { get; set; } //data time
+    public int sales { get; set; } //totale sales for a drink
+}
---
+++ SomerenService/RevenueService.cs
@@ -0,0 +1,25 @@
+﻿using SomerenDAL;
+using SomerenModel;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+
+public class RevenueService
+{
+    // creating field revenuedb form class RevenueDao
+    private RevenueDAO revenuedb;
+
+    public RevenueService()
+    {
+        // new RevenueDAO from RevenueDAO revenuedb
+        revenuedb = new RevenueDAO();
+    }
+
+    public List<Revenue> GetRevenues(DateTime selectedDate)
+    {
+        // getting revenue data from the database
+        List<Revenue> revenues = revenuedb.GetAllRevenuesByDate(selectedDate);
+        return revenues;
+    }
+}
